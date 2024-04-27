43fba1991d6ac64321bb0555b38e5587fb7f6109
Dibyajyoti Dhar 🤖 <didhar@paypal.com>
2024-04-27 20:37:47 +0530

    springboot init first commit

---
+++ usages/src/main/java/com/redis/all/RedisServerAllUsagesApplication.java
@@ -0,0 +1,13 @@
+package com.redis.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisServerAllUsagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisServerAllUsagesApplication.class, args);
+	}
+
+}
---
+++ usages/src/test/java/com/redis/all/RedisServerAllUsagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisServerAllUsagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
